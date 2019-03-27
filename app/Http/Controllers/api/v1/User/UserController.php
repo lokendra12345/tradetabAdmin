b3cddbde288512bc9ab485cb7a4722d982c1b204
lokendra <tech@iroute.in>
2019-03-27 09:40:34 +0530

    firts commit

---
+++ app/Http/Controllers/api/v1/User/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\api\v1\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+//use Validator;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth; 
+
+class UserController extends Controller
+{
+    //
+
+	public $successStatus = 200;
+
+/** 
+     * login api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */ 
+    public function login(){ 
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+            return response()->json(['success' => $success], $this-> successStatus); 
+        } 
+        else{ 
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        } 
+    }
+/** 
+     * Register api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */ 
+    public function register(Request $request) 
+    { 
+        $validator = Validator::make($request->all(), [ 
+            'first_name' => ['required', 'string', 'max:255'], 
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6'], 
+            //'password' => ['required', 'string', 'min:6', 'confirmed'],
+            //'c_password' => 'required|same:password', 
+             'mobile' => 'required|min:11|numeric',
+             'user_type' => 'required',
+             'status' => 'required', 
+        ]);
+	if ($validator->fails()) { 
+	            return response()->json(['error'=>$validator->errors()], 401);            
+	        }
+	$input = $request->all(); 
+	        $input['password'] = bcrypt($input['password']); 
+	        $user = User::create($input); 
+	        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+	        //$success[] =  $user;
+	        $success['first_name'] =  $user->first_name;
+	        $success['last_name'] =  $user->last_name;
+	        $success['email'] =  $user->email;
+	        $success['mobile'] =  $user->mobile;
+	        $success['user_type'] =  $user->user_type;
+	        $success['status'] =  $user->status;
+	return response()->json(['success'=>$success], $this-> successStatus); 
+	    }
+
+
+}
---
+++ app/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        //'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'email_verified_at', 'password', 'mobile', 'gmail_id', 'fb_id', 'linkedin_id', 'user_type', 'status', 'remember_token', 'created_at', 'updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
